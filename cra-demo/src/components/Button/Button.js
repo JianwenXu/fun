d3789afcef520e3f3659a8704ebaaa7e7ac232a7
xujianwen <xujianwen@ruijie.com.cn>
2019-12-11 11:38:01 +0800

    改变cra package.json scripts

--- cra-demo/src/components/Button/Button.js
+++ cra-demo/src/components/Button/Button.js
@@ -6,18 +6,18 @@ import './Button.css';
 /**
  * The only true button.
  */
-const Button = ({ color, size, onClick, disabled, children }) => {
+export default function Button({ color, size, onClick, disabled, children }) {
 	const styles = {
 		color,
-		fontSize: Button.sizes[size]
+		fontSize: Button.sizes[size],
 	};
+
 	return (
 		<button className="button" style={styles} onClick={onClick} disabled={disabled}>
 			{children}
 		</button>
 	);
 }
-
 Button.propTypes = {
 	/** Button label */
 	children: PropTypes.node.isRequired,
@@ -30,7 +30,6 @@ Button.propTypes = {
 	/** Gets called when the user clicks on the button */
 	onClick: PropTypes.func,
 };
-
 Button.defaultProps = {
 	color: '#333',
 	size: 'normal',
@@ -39,11 +38,8 @@ Button.defaultProps = {
 		console.log('You have clicked me!', event.target);
 	},
 };
-
 Button.sizes = {
 	small: '10px',
 	normal: '14px',
 	large: '18px',
 };
-
-export default Button;
